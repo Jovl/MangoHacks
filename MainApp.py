c182d49441b87d7d731dc194f7414f7b7bb650ed
Jeremiah <jeremiahlantzer25@gmail.com>
2017-04-01 12:43:10 -0400

    Update MainApp.py

--- MainApp.py
+++ MainApp.py
@@ -5,8 +5,8 @@ Author: Jeremiah Lantzer
 
 Travel reccomendations on a budget.
 
-Insta Auth Token Jeremiah: 42404802.114b636.262414e1e9324e88a8d1de70562b3039
-Insta Auth Token Kelly: 22220287.114b636.9b53377e1cb84493a59a7950b16795bd
+Insta Auth Token Jeremiah: XXXXXXX
+Insta Auth Token Kelly: XXXXXXX
 """
 
 from flask import Flask, jsonify
